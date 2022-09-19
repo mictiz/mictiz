a133c80c5cc166c8b1cc9698fa712fd5603f6178
mic tiz <mictiz@gmail.com>
2022-09-19 15:13:00 +0200

    Create local preview script

---
+++ view.sh
@@ -0,0 +1,10 @@
+cat << EOF > view.sh
+#!/usr/bin/env bash
+
+if [ -d "resources" ]; then
+  rm -Rf resources
+fi
+
+hugo --i18n-warnings server --watch --disableFastRender --buildDrafts --baseUrl ""
+
+EOF
